216be4882ae607250cb7d533b0db5ac4bf6ec55e
Magesh Kuppan <tkmagesh77@gmail.com>
2025-06-05 23:19:04 +0530

    introducing promises

--- 02-async/script.js
+++ 02-async/script.js
@@ -1,42 +1,77 @@
 (function(){
-    // sync
-    function add(x,y){
-        console.log("   [add] operation started")
-        var result = x + y;
-        console.log("   [add] operation completed");
-        return result
-    }
+  // sync
+  function add(x, y) {
+    console.log("   [add] operation started");
+    var result = x + y;
+    console.log("   [add] operation completed");
+    return result;
+  }
 
-    function addClient(x,y){
-        console.log('[addClient] operation initiated')
-        var result = add(100,200)
-        console.log('result =', result)
-        console.log("[addClient] operation done");
-    }
+  function addClient(x, y) {
+    console.log("[addClient] operation initiated");
+    var result = add(100, 200);
+    console.log("result =", result);
+    console.log("[addClient] operation done");
+  }
 
-    window['addClient'] = addClient
+  window["addClient"] = addClient;
 
-    // async
-    function addAsync(x, y, callbackFn) {
-      console.log("   [add] operation started");
-      setTimeout(() => {
-        var result = x + y;
-        console.log("   [add] operation completed");
-        // return result;  
-        callbackFn(result)
-      }, 4000);
-      
-    }
+  // async (using callbacks)
+  function addAsync(x, y, callbackFn) {
+    console.log("   [add] operation started");
+    setTimeout(() => {
+      var result = x + y;
+      console.log("   [add] operation completed");
+      // return result;
+      callbackFn(result);
+    }, 4000);
+  }
 
-    function addAsyncClient(x, y) {
-      console.log("[addClient] operation initiated");
-      addAsync(x, y, function(result){
-        console.log("result =", result);
-        console.log("[addClient] operation done");
-      });
-      
-    }
+  function addAsyncClient(x, y) {
+    console.log("[addClient] operation initiated");
+    addAsync(x, y, function (result) {
+      console.log("result =", result);
+      console.log("[addClient] operation done");
+    });
+  }
+  window["addAsyncClient"] = addAsyncClient;
 
-    window["addAsyncClient"] = addAsyncClient;
+  // async (using Promise)
+  function addAsyncPromise(x, y) {
+    console.log("   [add] operation started");
+    var p = new Promise(function(resolveFn, rejectFn){
+        setTimeout(() => {
+          var result = x + y;
+          resolveFn(result);
+          console.log("   [add] operation completed");
+        }, 4000);
+    })
+    return p;
+  }
 
+  window["addAsyncPromise"] = addAsyncPromise;
+
+  function addAsyncPromiseClient(x, y) {
+    console.log("[addClient] operation initiated");
+    var p = addAsyncPromise(x, y)
+    /* 
+    p.then(function (result) {
+      console.log("result =", result);
+      console.log("[addClient] operation done");
+    }); 
+    */
+   return p
+  }
+
+//   window["addAsyncPromiseClient"] = addAsyncPromiseClient;
+
+  function calculatorView(){
+    var p = addAsyncPromiseClient(100,200)
+    p.then(function (result) {
+      console.log("result =", result);
+      console.log("[addClient] operation done");
+    }); 
+  }
+//   window["addAsyncPromiseClient"] = addAsyncPromiseClient;
+  window["calculatorView"] = calculatorView;
 })()
